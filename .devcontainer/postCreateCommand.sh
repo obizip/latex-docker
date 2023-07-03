42aa298c1e79949788b49a2c531a1b2a89156b38
obizip <ct4kuto@gmail.com>
2023-07-03 14:31:24 +0900

    Add python

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,6 @@
+ln -snf /doc/.latexmkrc ~/.latexmkrc
+
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+pip3 install torchviz
+pip3 install ipykernel jupyter ipython
+pip3 install numpy pandas matplotlib scipy
